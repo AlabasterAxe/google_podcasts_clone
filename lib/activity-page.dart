8945a2cb037579af0ea1ee9374504088b2497210
Matthew Keller <matthew.ed.keller@gmail.com>
2020-12-23 10:31:50 -0500

    add Activity SliverAppBar

--- lib/activity-page.dart
+++ lib/activity-page.dart
@@ -6,6 +6,19 @@ class ActivityPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Icon(Icons.playlist_play);
+    return CustomScrollView(slivers: [
+      SliverAppBar(
+        leading: Icon(Icons.cast, color: Colors.grey[900]),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        title: Text("Activity", style: TextStyle(color: Colors.grey[900])),
+        floating: true,
+        actions: [
+          Icon(Icons.edit, color: Colors.grey[900]),
+          Icon(Icons.more_vert, color: Colors.grey[900])
+        ],
+      ),
+      SliverList(delegate: SliverChildListDelegate([]))
+    ]);
   }
 }
